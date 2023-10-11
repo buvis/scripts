014c9f565d3073b1fb432771dd29569b4d392638
Tomáš Bouška <tomas@buvis.net>
2023-10-11 17:53:41 +0200

    Improve console

--- src/buvis/adapters/console/console.py
+++ src/buvis/adapters/console/console.py
@@ -2,7 +2,11 @@ from rich.console import Console
 from rich.prompt import Confirm
 
 CHECKMARK = "[bold green1]\u2714[/bold green1]"
+WARNING = "[bold orange3]\u26A0[/bold orange3]"
 CROSSMARK = "[bold indian_red]\u2718[/bold indian_red]"
+STYLE_SUCCESS_MSG = "spring_green1"
+STYLE_WARNING_MSG = "light_goldenrod3"
+STYLE_FAILURE_MSG = "bold light_salmon3"
 
 
 class ConsoleAdapter:
@@ -10,15 +14,27 @@ class ConsoleAdapter:
     def __init__(self):
         self.console = Console(log_path=False)
 
+    def format_success(self, message):
+        return f" {CHECKMARK} [{STYLE_SUCCESS_MSG}]{message}[/{STYLE_SUCCESS_MSG}]"
+
     def success(self, message):
-        self.console.print(f"{CHECKMARK} {message}")
+        self.console.print(self.format_success(message))
+
+    def format_warning(self, message):
+        return f" {WARNING} [{STYLE_WARNING_MSG}]{message}[/{STYLE_WARNING_MSG}]"
+
+    def warning(self, message):
+        self.console.print(self.format_warning(message))
+
+    def format_failure(self, message, details=""):
+        formatted_message = f" {CROSSMARK} [{STYLE_FAILURE_MSG}]{message}[/{STYLE_FAILURE_MSG}]"
+        if details:
+            formatted_message += " \n\n Details:\n\n {details}"
+
+        return formatted_message
 
     def failure(self, message, details=""):
-        if details:
-            self.console.print(
-                f"{CROSSMARK} {message} \n\nDetails:\n\n {details}")
-        else:
-            self.console.print(f"{CROSSMARK} {message}")
+        self.console.print(self.format_failure(message, details))
 
     def panic(self, message, details=""):
         self.failure(message, details)
@@ -33,8 +49,11 @@ class ConsoleAdapter:
     def confirm(self, message):
         return Confirm.ask(message)
 
-    def input_password(self, prompt="Enter password"):
-        return self.console.input(prompt, password=True)
+    def print(self, message):
+        return self.console.print(message)
+
+    def nl(self):
+        return self.console.out("")
 
 
 console = ConsoleAdapter()
