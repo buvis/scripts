73aab8d026735299457cb4a24fc300b2ffb78287
Tomáš Bouška <tomas.bouska@adp.com>
2024-04-09 18:10:40 +0200

    Undo local changes before updating submodules

--- bin/cfgl.bat
+++ bin/cfgl.bat
@@ -1,3 +1,4 @@
 git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska pull
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule foreach git reset --hard
 git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --init
 git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --remote --merge
