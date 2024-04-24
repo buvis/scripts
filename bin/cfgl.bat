6a5997df47171b698bf42605cc59d654d02e20da
Tomáš Bouška <tomas.bouska@adp.com>
2024-04-24 16:22:24 +0200

    Delete obsolete scripts

--- bin/cfgl.bat
+++
@@ -1,4 +0,0 @@
-git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska pull
-git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule foreach git reset --hard
-git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --init
-git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --remote --merge
