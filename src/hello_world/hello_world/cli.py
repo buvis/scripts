0400be5b68824d1205c76afd57e1d0d4b9a1354a
Tomáš Bouška <tomas@buvis.net>
2024-08-09 20:53:19 +0200

    Improve font selection in hello_world

--- src/hello_world/hello_world/cli.py
+++ src/hello_world/hello_world/cli.py
@@ -1,37 +1,57 @@
 from pathlib import Path
 
 import click
+import pyfiglet
 
 from buvis.adapters import ConfigAdapter
 
 from hello_world.commands import CommandPrintFiglet
 
 try:
-    cfg = ConfigAdapter(Path(__file__,"../../config.yaml"))
+    cfg = ConfigAdapter(Path(__file__, "../../config.yaml"))
     res = cfg.get_key_value("figlet_font")
 
     if res.is_ok():
         DEFAULT_FONT = res.payload
     else:
-        DEFAULT_FONT = 'doom'
+        DEFAULT_FONT = "doom"
 except FileNotFoundError:
     cfg = ConfigAdapter()
-    DEFAULT_FONT = 'doom'
+    DEFAULT_FONT = "doom"
 
-DEFAULT_TEXT = 'World'
+DEFAULT_TEXT = "World"
+
 
 @click.group(help="CLI tool as script proof of concept", invoke_without_command=True)
-@click.option("-f","--font", default=DEFAULT_FONT, help="Font to use for stylized printing.")
+@click.option(
+    "-f", "--font", default=DEFAULT_FONT, help="Font to use for stylized printing."
+)
+@click.option(
+    "-l",
+    "--list-fonts",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="List available fonts",
+)
 @click.argument("text", default=DEFAULT_TEXT)
 def cli(
     text: str = DEFAULT_TEXT,
     font: str = DEFAULT_FONT,
+    list_fonts: bool = False,
 ):
-    cfg.set_key_value("font", font)
-    cfg.set_key_value("text", text)
+    if list_fonts:
+        print("\n".join(sorted(pyfiglet.FigletFont.getFonts())))
+    else:
+        if font in pyfiglet.FigletFont.getFonts():
+            cfg.set_key_value("font", font)
+        else:
+            cfg.set_key_value("font", DEFAULT_FONT)
 
-    cmd = CommandPrintFiglet(cfg)
-    cmd.execute()
+        cfg.set_key_value("text", text)
+
+        cmd = CommandPrintFiglet(cfg)
+        cmd.execute()
 
 
 if __name__ == "__main__":
