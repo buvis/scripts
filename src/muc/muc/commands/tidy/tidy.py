7f1398218dcd604ca6774cc7d7535403acbf973e
Tomáš Bouška <tomas@buvis.net>
2024-11-06 16:36:11 +0100

    Implement dot_clean behavior

--- src/muc/muc/commands/tidy/tidy.py
+++ src/muc/muc/commands/tidy/tidy.py
@@ -33,6 +33,7 @@ class CommandTidy:
             raise ConfigurationKeyNotFoundError from e
 
     def execute(self: "CommandTidy") -> None:
+        DirTree.merge_mac_metadata(self.dir)
         DirTree.normalize_file_extensions(self.dir)
         DirTree.delete_by_extension(self.dir, self.junk_extensions)
         DirTree.remove_empty_directories(self.dir)
